0497a6a5af9814b6bfade953cc8329ac55e7e559
yoshikatsu.shimizu <yoshikatsu.shimizu@cap-net.co.jp>
2019-02-06 15:19:25 +0900

    元号データを外部ファイル化

--- JapaneseEraClassLibrary/JapaneseEra.cs
+++ JapaneseEraClassLibrary/JapaneseEra.cs
@@ -1,88 +1,112 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace JapaneseEraClassLibrary
 {
     /// <summary>
     /// 和暦クラスです。
     /// </summary>
-    public class JapaneseEra
+    public static class JapaneseEra
     {
-        /// <summary>
-        /// 元号の開始日 
-        /// </summary>
-        public DateTime StartDate { get; }
+
+        [System.Xml.Serialization.XmlElement("EraTable")]
+        private static readonly EraTable table;
 
         /// <summary>
-        ///  元号の名称 
+        /// 静的コンストラクタ
         /// </summary>
-        public string Name { get; }
-
-        /// <summary>
-        /// 元号の頭文字
-        /// </summary>
-        public string FirstLetter { get; }
-
-        /// <summary>
-        /// ローマ字表記の名称
-        /// </summary>
-        public string EnglishName { get; }
-
-        /// <summary>
-        /// ローマ字表記の頭文字
-        /// </summary>
-        public string EnglishFirstLetter { get; }
-
-        /// <summary>
-        /// コンストラクタ
-        /// </summary>
-        /// <param name="startDate"></param>
-        /// <param name="name"></param>
-        /// <param name="englishName"></param>
-        private JapaneseEra(DateTime startDate, string name, string englishName)
+        static JapaneseEra()
         {
-            StartDate = startDate;
-            Name = name;
-            FirstLetter = Name.Substring(0, 1);
-            EnglishName = englishName;
-            EnglishFirstLetter = EnglishName.Substring(0, 1);
+            using (FileStream fs = new System.IO.FileStream(Directory.GetCurrentDirectory() + @"\\EraTable.xml", System.IO.FileMode.Open))
+            {
+                try
+                {
+                    System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(EraTable));
+                    table = (EraTable)serializer.Deserialize(fs);
+                }
+                catch (Exception ex)
+                {
+                    throw new IOException("xmlファイルの読込に失敗しました。", ex);
+                }
+            }
         }
-
-        /// <summary>
-        /// 静的プロパティ：JapaneseEraインスタンスを収めた読み取り専用のリスト 
-        /// </summary>
-        public static IList<JapaneseEra> Table { get; }
-          = new List<JapaneseEra>() // 順序が保証されるコレクションを使う
-          {
-              // 必ず降順に並べる
-              new JapaneseEra(new DateTime(2019,5,1), "新元", "Shingen"),
-              new JapaneseEra(new DateTime(1989,1,8), "平成", "Heisei"),
-              new JapaneseEra(new DateTime(1926,12,25), "昭和", "Showa"),
-              new JapaneseEra(new DateTime(1912,7,30), "大正", "Taisho"),
-              new JapaneseEra(new DateTime(1868,1,25), "明治", "Meiji"),
-          }
-          .AsReadOnly(); // リストの内容を変更できないようにする
 
         /// <summary>
         /// 日付に該当するJapaneseEraインスタンスを得る 
         /// </summary>
         /// <param name="theDate"></param>
         /// <returns></returns>
-        private static JapaneseEra GetEra(DateTime theDate)
-          => Table.FirstOrDefault(era => (era.StartDate <= theDate));
+        private static Era GetEra(DateTime theDate)
+          => table.eras.FirstOrDefault(era => (era.StartDateTime.Date <= theDate.Date && theDate.Date <= era.EndDateTime.Date));
 
         /// <summary>
         /// 日付から和暦の年月日を得る
         /// </summary>
         /// <param name="theDate"></param>
         /// <returns></returns>
-        public static string GetEraYMD(DateTime theDate)
+        public static string GetYMD(DateTime theDate)
         {
             var e = GetEra(theDate);
-            return $"{e.Name}{(theDate.Year - e.StartDate.Year + 1):00}年{theDate:MM月dd日}";
+            return $"{e.Name}{(theDate.Year - e.StartDateTime.Year + 1):00}年{theDate:MM月dd日}";
+        }
+
+    }
+
+    [System.Xml.Serialization.XmlRoot("EraTable")]
+    public class EraTable
+    {
+        /// <summary>
+        /// 人
+        /// </summary>
+        [System.Xml.Serialization.XmlElement("Era")]
+        public List<Era> eras { get; set; }
+    }
+
+    public class Era
+    {
+        /// <summary>
+        /// 元号の開始日 
+        /// </summary>
+        [System.Xml.Serialization.XmlElement("StartDate")]
+        public string StartDate { get; set; }
+
+        /// <summary>
+        /// 元号の開始日 
+        /// </summary>
+        [System.Xml.Serialization.XmlElement("EndDate")]
+        public string EndDate { get; set; }
+
+        /// <summary>
+        ///  元号の名称 
+        /// </summary>
+        [System.Xml.Serialization.XmlElement("Name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// ローマ字表記の名称
+        /// </summary>
+        [System.Xml.Serialization.XmlElement("EnglishName")]
+        public string EnglishName { get; set; }
+
+        [System.Xml.Serialization.XmlIgnore]
+        public DateTime StartDateTime
+        {
+            get{
+                return DateTime.Parse(this.StartDate);
+            }
+        }
+
+        [System.Xml.Serialization.XmlIgnore]
+        public DateTime EndDateTime
+        {
+            get
+            {
+                return String.IsNullOrEmpty(this.EndDate) ? DateTime.MaxValue : DateTime.Parse(this.EndDate);
+            }
         }
     }
+
+
 }
---
+++ JapaneseEraClassLibrary/main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JapaneseEraClassLibrary
+{
+    class main
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Console.WriteLine(JapaneseEra.GetYMD(new DateTime(2019, 04, 01)));
+
+            //var a = new JapaneseEra();
+
+            Console.WriteLine(DateTime.Parse("1868-01-25"));
+        }
+    }
+}
